25fcac940308271d654e2a4c5bfb3d91182fbf06
Aiman Shafi <shafiaiman88@gmail.com>
2023-08-31 01:41:56 +0600

    boostrap project

---
+++ ProjectTailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      heading: "#1e293b",
+      primary: "#3a83f6",
+      secordary: "#4ba1f0",
+      blue: "#3a82f6",
+      white: "#ffffff",
+      darkblue: "#1d4ed8",
+      darkgray: "#030712",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
