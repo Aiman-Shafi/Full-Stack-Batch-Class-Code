1e9640ab659a6ca532dc5f18a8342d79fd80dd4b
Aiman Shafi <shafiaiman88@gmail.com>
2023-10-12 01:00:34 +0600

    word app

---
+++ Word Analytics/app.js
@@ -0,0 +1,53 @@
+// No. of word count
+// No. characters count
+// Limit -> linkedin & twitter
+// warning for some tags or keywords
+
+// selectors
+
+const textAreaElement = document.querySelector(".word-counter-textarea");
+const wordCounter = document.querySelector(".words");
+const characterCounter = document.querySelector(".characters");
+const twitterCounter = document.querySelector(".twitter");
+const linkedinCounter = document.querySelector(".linkedin");
+
+const invalidWordsList = ["payment", "list", "script"];
+
+const wordCounterHandler = () => {
+  // input validation
+
+  invalidWordsList.forEach((word) => {
+    if (textAreaElement.value.includes(word)) {
+      alert(`you cannot provide any ${word} here!`);
+      textAreaElement.value = textAreaElement.value.replace(word, "");
+    }
+  });
+
+  // setting number of characters
+  let numberOfCharacters = textAreaElement.value.length;
+  characterCounter.innerHTML = numberOfCharacters;
+  // determine number of words
+  let numberOfWords = textAreaElement.value.split(" ").length;
+  if (textAreaElement.value.length === 0) {
+    numberOfWords = 0;
+  }
+  wordCounter.innerHTML = numberOfWords;
+
+  // twitter counter limit set
+  let twitterCharactersLeft = 250 - numberOfCharacters;
+  twitterCounter.innerHTML = twitterCharactersLeft;
+
+  // twitter counter limit set
+  let linkedinCharactersLeft = 320 - numberOfCharacters;
+  linkedinCounter.innerHTML = linkedinCharactersLeft;
+
+  if (twitterCharactersLeft < 0) {
+    twitterCounter.innerHTML = 0;
+  }
+
+  if (linkedinCharactersLeft < 0) {
+    linkedinCounter.innerHTML = 0;
+  }
+};
+
+textAreaElement.addEventListener("input", wordCounterHandler);
